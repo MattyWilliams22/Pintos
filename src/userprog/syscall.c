fb5ef9a53929a29ad188e74947cf375cf0f806f6
Matty Williams <mw2122@doc.ic.ac.uk>
2023-11-05 18:52:51 +0000

    Added templates for system call functions

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,7 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "devices/shutdown.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -17,4 +18,90 @@ syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
   thread_exit ();
+}
+
+void
+halt (void) {
+  shutdown_power_off();
+}
+
+void
+exit (int status) 
+{
+  // Incomplete
+}
+
+pid_t
+exec (const char *cmd_line) 
+{
+  // Must split up cmd_line to get file name
+  return (pid_t) process_execute(cmd_line);
+}
+
+int
+wait (pid_t pid) 
+{
+  // Incomplete
+  return process_wait(pid);
+}
+
+bool 
+create (const char *file, unsigned initial_size) 
+{
+  // Incomplete
+  return false;
+}
+
+bool
+remove (const char *file) 
+{
+  // Incomplete
+  return false;
+}
+
+int
+open (const char *file) 
+{
+  // Incomplete
+  return -1;
+}
+
+int 
+filesize (int fd) 
+{
+  // Incomplete
+  return -1;
+}
+
+int 
+read (int fd, void *buffer, unsigned size) 
+{
+  // Incomplete
+  return -1;
+}
+
+int
+write (int fd, const void *buffer, unsigned size)
+{
+  // Incomplete
+  return -1;
+}
+
+void
+seek (int fd, unsigned position) 
+{
+  // Incomplete
+}
+
+unsigned
+tell (int fd)
+{
+  // Incomplete
+  return 0;
+}
+
+void
+close (int fd) 
+{
+  // Incomplete
 }
